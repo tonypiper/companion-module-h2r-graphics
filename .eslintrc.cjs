b7b0a4f76bcf5975967d40880ba989f4ada68364
Tony Piper <tpiper@tpiper.com>
2024-02-26 19:05:05 +0000

    install jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,11 @@
 module.exports = {
-	extends: './node_modules/@companion-module/tools/eslint/main.cjs',
+	extends: ['./node_modules/@companion-module/tools/eslint/main.cjs'],
+	overrides: [
+		{
+			files: ['tests/**/*.js'],
+			env: {
+				jest: true,
+			},
+		},
+	],
 }
